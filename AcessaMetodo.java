af017fdd4d3bc2a32c17dda0c72787780e1fc2c4
jfsilvadelima <jfsilvadelima@gmail.com>
2019-03-07 19:44:40 -0300

    Add files via upload

---
+++ AcessaMetodo.java
@@ -0,0 +1,15 @@
+package teste_;
+
+public class AcessaMetodo {
+
+	 public void imprime(){
+	        System.out.println("Bem Vindo ao Java!");
+	    }
+	 
+	    public static void main(String[] args) {
+	        AcessaMetodo acessa = new AcessaMetodo ();
+	        acessa.imprime();
+	 
+	    }
+	 
+}
