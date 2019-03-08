d7e2e83a4c07f321b44be3a9152fb975a0e6f8bb
jfsilvadelima <jfsilvadelima@gmail.com>
2019-03-08 11:10:51 -0300

    Add files via upload

---
+++ Gerente.java
@@ -0,0 +1,21 @@
+package lp2;
+
+public class Gerente {
+    private String nome;
+    private String cpf;
+    private double salario;
+    private int senha;
+    private int numeroDeFuncionariosGerenciados;
+
+    public boolean autentica(int senha) {
+        if (this.senha == senha) {
+            System.out.println("Acesso Permitido!");
+            return true;
+        } else {
+            System.out.println("Acesso Negado!");
+            return false;
+        }
+    }
+
+    // outros métodos 
+}
